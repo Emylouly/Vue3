2ed2da7fc5ff7d53e8dc367365a006d84345ced5
Emylouly <emilylouisemika@gmail.com>
2025-02-18 16:02:31 -0300

    Revert "Update App.vue"
    This reverts commit fd40dc5fcf57f21b6d1190a502112e01a8e6a926.

--- 3_vue_cli/src/App.vue
+++ 3_vue_cli/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <primeiro-componente />
-  <segundo-componente />
+  <PrimeiroComponente />
+  <SegundoComponente />
 
 </template>
   
 <script>
   
-  import PrimeiroComponente from './components/PrimeiroComponente.vue';
-import SegundoComponente from './components/SegundoComponente.vue';
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import SegundoComponente from './components/SegundoComponente.vue'
   
   export default{
       name: 'App',
