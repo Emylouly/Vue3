d188b1cbccc9dc53d028fd107d115b6615bf8ed1
Emylouly <emilylouisemika@gmail.com>
2025-02-18 15:04:59 -0300

    a

--- 3_vue_cli/src/App.vue
+++ 3_vue_cli/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PrimeiroComponente />
+  <SegundoComponente />
+
 </template>
-
+  
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import SegundoComponente from './components/SegundoComponente.vue'
+  
+  export default{
+      name: 'App',
+      components:{
+          PrimeiroComponente,
+          SegundoComponente
+          
+      }
   }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ 3_vue_cli/src/components/SegundoComponente.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Café com leite</h1>
+</template>
+
+<script>
+
+export default{
+    name:'SegundoComponente'
+}
+
+</script>
