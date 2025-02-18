e3213400becadffc7359e9eb0d12a05c3b823f8b
Emylouly <emilylouisemika@gmail.com>
2025-02-18 16:21:01 -0300

    aaa

--- 3_vue_cli/src/App.vue
+++ 3_vue_cli/src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <PrimeiroComponente />
-  <SegundoComponente />
-
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
-  
-<script>
-  
-  import PrimeiroComponente from './components/PrimeiroComponente.vue'
-  import SegundoComponente from './components/SegundoComponente.vue'
-  
-  export default{
-      name: 'App',
-      components:{
-          PrimeiroComponente,
-          SegundoComponente
-          
-      }
-  }
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
