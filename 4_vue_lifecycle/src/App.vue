d2654d94afbca001cc94d7121a580a6fa5107320
Emylouly <emilylouisemika@gmail.com>
2025-02-18 16:59:49 -0300

    e

---
+++ 4_vue_lifecycle/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <primeiro-componente/>
+
+<p>O dado de app é {{ testando }}</p>
+
+  </div>
+</template>
+
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue';
+
+export default{
+  name:"App",
+  components:{
+    PrimeiroComponente,
+  },
+  data(){
+    return{
+        testando: "Teste",
+    }
+  }
+}
+
+</script>
---
+++ 4_vue_lifecycle/src/components/PrimeiroComponente.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Ola mundo</h1>
+</template>
+
+<script>
+export default{
+    name:"PrimeiroComponente",
+    data(){
+        return{
+            nome:"Emily",
+            profissao:"Sla"
+        }
+    }
+}
+</script>
