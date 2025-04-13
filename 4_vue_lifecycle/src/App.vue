f673aae5ed5a72001ade748c08fce29b1c8b9176
Emylouly <emilylouisemika@gmail.com>
2025-04-13 11:49:56 -0300

    a

--- 4_vue_lifecycle/src/App.vue
+++ 4_vue_lifecycle/src/App.vue
@@ -1,9 +1,22 @@
 <template>
 <div>
 
+  <p v-if="esta_trabalhando">Sono e cansaço</p>
+  <p v-else>To com fome</p>
+
+  <select v-model="mostrar_email">
+    <option :value="true">Mostrar</option>
+    <option :value="false">Esconder</option>
+  </select>
+  <p v-show="mostrar_email">Meu email é: {{ email }}</p>
+
+  <p>Acesse o google: <a v-bind:href="meu_link" target="Google1">Google omg</a></p>
+
   <PrimeiroComponente />
 
   <p>O dado de app é {{ testando }}</p>
+
+  <Picture/>
 
   <LyfeCycle />
 
@@ -16,6 +29,7 @@
 
 import LyfeCycle from './components/LyfeCycle.vue';
 import Pessoas from './components/Pessoas.vue';
+import Picture from './components/Picture.vue';
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 
 export default {
@@ -24,10 +38,15 @@ export default {
     PrimeiroComponente,
     LyfeCycle,
     Pessoas,
+    Picture
   },
   data(){
     return{
         testando: 'Tste',
+        esta_trabalhando: true,
+        email:'emily@gmail.com',
+        mostrar_email: true,
+        meu_link:'https://google.com'
     }
   },
 }
---
+++ 4_vue_lifecycle/src/components/Picture.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+        <img :src="mosquichorro" :alt="descricao">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Picture',
+    data(){
+        return{
+            mosquichorro:"/img/a.png",
+            descricao:"um cachorro mosquito"
+        }
+    }
+}
+</script>
