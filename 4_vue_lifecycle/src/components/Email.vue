faec6e4ac966b11333e3108584b7175fbe16c91b
Emylouly <emilylouisemika@gmail.com>
2025-04-23 14:35:26 -0300

    Update Email.vue

--- 4_vue_lifecycle/src/components/Email.vue
+++ 4_vue_lifecycle/src/components/Email.vue
@@ -1,14 +1,10 @@
 <template>
     <div>
-        <div>
-        <button @click="showEmail">Mostrar</button>
-        </div>
-        <p v-show="mostrar">{{ email }}</p>
-
-        <div>
-            <button @click="showTrabalho">{{ textoBotao }}</button>
-        </div>
-        <p v-show="mostrar2">{{ trabalho }}</p>
+        <select v-model="mostrar">
+            <option :value="true">Mostra</option>
+            <option :value="false">Some</option>
+        </select>
+        <p>{{ email }}</p>
     </div>
 </template>
 
@@ -16,33 +12,8 @@
 
 export default {
     name:'Email',
-    props: {
+    props:{
         email: String,
-        trabalho: String,
-    },
-    data(){
-        return{
-            mostrar: false,
-            mostrar2: false,
-            textoBotao: 'Mostrar trabalho',
-        }
-    },
-    methods:{
-        showEmail(){
-            console.log("Foi"),
-            this.mostrar = !this.mostrar;
-        },
-
-        showTrabalho(){
-            console.log("Rapaz"),
-            this.mostrar2 = !this.mostrar2;
-            if(!this.mostrar2){
-                this.textoBotao = 'Mostrar trabalho'
-            }
-            else{
-                this.textoBotao = 'Esconder Trabalho'
-            }
-        }
     }
 }
 </script>
