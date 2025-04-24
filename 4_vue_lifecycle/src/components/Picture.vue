9faea673a593cfcb44cf165ed50eaa527c312c66
Emylouly <emilylouisemika@gmail.com>
2025-04-24 11:31:20 -0300

    a

--- 4_vue_lifecycle/src/components/Picture.vue
+++ 4_vue_lifecycle/src/components/Picture.vue
@@ -2,17 +2,37 @@
     <div>
 
         <img :src="mosquichorro" :alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem" />
 
     </div>
 </template>
 
 <script>
+
+import MudarImagem from './MudarImagem.vue';
+
 export default {
     name:'Picture',
+    components: {
+        MudarImagem
+    },
     data(){
         return{
             mosquichorro:"/img/a.png",
             descricao:"um cachorro mosquito"
+        }
+    },
+    methods: {
+        trocarImagem() {
+            if(this.mosquichorro == '/img/a.png'){
+                this.mosquichorro = '/img/b.png'
+            }
+            else if(this.mosquichorro == '/img/b.png'){
+                this.mosquichorro = '/img/c.png'
+            }
+            else {
+                this.mosquichorro = '/img/a.png'
+            }
         }
     }
 }
