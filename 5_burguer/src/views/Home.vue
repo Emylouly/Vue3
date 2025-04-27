2f85e80b9b432db03d729edca89e842c67ca4e3f
Emylouly <emilylouisemika@gmail.com>
2025-04-27 12:01:38 -0300

    foi

--- 5_burguer/src/views/Home.vue
+++ 5_burguer/src/views/Home.vue
@@ -3,18 +3,22 @@
     <Banner />
     <div class="main-container">
     <h1>Monte o seu burguer</h1>
-    <p>Formulário</p>
+    <BurgerForm />
   </div>
   </div>
 </template>
 
+
 <script>
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
+
 
 export default {
   name: 'Home',
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>
