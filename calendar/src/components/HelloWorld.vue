ec8ee0ac2aa05955e80d21dfdaf952c7cae0217d
Emylouly <emilylouisemika@gmail.com>
2025-05-07 11:13:07 -0300

    a

---
+++ calendar/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container class="my-5">
+    <v-row justify="center">
+      <v-col cols="12" md="5">
+        <!-- Cartão principal -->
+        <v-card elevation="4">
+          <v-card-title class="text-h5">Atalho do Teclado</v-card-title>
+          <v-card-text>
+            <v-sheet class="pa-4" color="#f5f5f5">
+              <p>Use o atalho abaixo para copiar o texto selecionado:</p>
+              <v-kbd>Ctrl</v-kbd> + <v-kbd>C</v-kbd>
+            </v-sheet>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" text>Ok, entendi</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-btn to="/home">Ir para home</v-btn>
+
+  </v-container>
+</template>
+
+<script>
+
+</script>
---
+++ calendar/src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container class="mt-10">
+      <v-row justify="center">
+        <v-col cols="12" md="8">
+          <v-card elevation="4">
+            <v-card-title class="text-h5">Página Inicial</v-card-title>
+            <v-card-text>
+              Esta é a página inicial criada com roteamento automático.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" @click="mostrarMensagem">Clique aqui</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-btn to="/">Ir para Início</v-btn>
+
+    </v-container>
+  </template>
+  
+  <script setup>
+  function mostrarMensagem() {
+    alert('Você clicou no botão!')
+  }
+  </script>
+  
